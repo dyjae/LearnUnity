b7174f3616a0fa13b0d31d4aeaec3c003cbf4d54
yj <dyjae@vip.qq.com>
2020-01-04 19:08:23 +0800

    feat 第5讲角色方向&跳跃  https://www.bilibili.com/video/av64181340

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour
     // 速度
     public float speed = 10f;
 
+    // 跳跃
+    public float jumpforce;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Debug.Log("移动了");
         Movement();
@@ -27,11 +30,20 @@ public class PlayerController : MonoBehaviour
     {
         //获取横向移动值
         float horizontalMove = Input.GetAxis("Horizontal");
+        float facedirection = Input.GetAxisRaw("Horizontal"); //直接获得  -1，0，1
         Debug.Log(horizontalMove);
         //不等于0时，表示有移动
         if (horizontalMove != 0)
         {
-            rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+            rb.velocity = new Vector2(horizontalMove * speed * Time.deltaTime, rb.velocity.y);
+        }
+        if(facedirection != 0)
+        {
+            transform.localScale = new Vector3(facedirection, 1, 1);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
         }
     }
 }
