4339b3be40feeda8b6815f6e1f39d361d3be327d
yj <dyjae@vip.qq.com>
2020-01-04 18:51:58 +0800

    feat 第四讲角色移动 https://www.bilibili.com/video/av64098866

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 角色刚体
+    public Rigidbody2D rb;
+
+    // 速度
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("开始了");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("移动了");
+        Movement();
+    }
+
+    void Movement()
+    {
+        //获取横向移动值
+        float horizontalMove = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalMove);
+        //不等于0时，表示有移动
+        if (horizontalMove != 0)
+        {
+            rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+        }
+    }
+}
