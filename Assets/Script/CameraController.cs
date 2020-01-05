e183acc93e3f81d17476429bf165133b1f45ac10
yj <dyjae@vip.qq.com>
2020-01-05 10:30:25 +0800

    feat 8 控制镜头大小

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    //玩家
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform为镜头
+        transform.position = new Vector3(player.position.x, player.position.y, -10);
+    }
+}
