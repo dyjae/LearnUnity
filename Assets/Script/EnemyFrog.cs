1ae5481001a9a1949afd2ece9b7521ad8cc9b8e4
yj <dyjae@vip.qq.com>
2020-01-05 19:51:05 +0800

    feat 15 AI敌人移动 https://www.bilibili.com/video/av65923429

---
+++ Assets/Script/EnemyFrog.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFrog : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    public Transform rightPoint,leftPoint;
+
+    public bool isFaceLeft = true;
+
+    public float speed = 10;
+
+    private float leftx, rightx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        transform.DetachChildren();
+        leftx = leftPoint.position.x;
+        rightx = rightPoint.position.x;
+        Destroy(rightPoint.gameObject);
+        Destroy(leftPoint.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveMent();
+    }
+
+    void MoveMent()
+    {
+        if (isFaceLeft)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            if(transform.position.x < leftPoint.position.x)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                isFaceLeft = false;
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            if (transform.position.x > rightPoint.position.x)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                isFaceLeft = true;
+            }
+        }
+    }
+
+}
