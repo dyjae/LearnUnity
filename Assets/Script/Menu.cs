957b809af5a392698ea4ba6b92919757f4a36759
yj <dyjae@vip.qq.com>
2020-01-27 20:24:30 +0800

    feat 主菜单MainMenu https://www.bilibili.com/video/av68253329

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayeGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void UIEnable()
+    {
+        GameObject.Find("Canvas/Menu/UI").SetActive(true);
+    }
+}
