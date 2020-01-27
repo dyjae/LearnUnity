b408ff3707a4b7af4f50ce702e2092f7280cb1bb
yj <dyjae@vip.qq.com>
2020-01-27 20:24:15 +0800

    feat 视觉差Parallax https://www.bilibili.com/video/av68090977/

---
+++ Assets/Script/Parallax.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Transform cam;
+
+    public float moveRate;
+
+    private float startPointX,startPointY;
+
+    public bool isLockY = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPointX = transform.position.x;
+        startPointY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLockY)
+        {
+            transform.position = new Vector2(startPointX + cam.position.x * moveRate, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(startPointX + cam.position.x * moveRate, startPointY+cam.position.y*moveRate);
+        }
+        
+    }
+}
