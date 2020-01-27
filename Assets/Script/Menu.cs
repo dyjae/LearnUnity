286017ac6c4985bc85271330de9115c9baf809ba
yj <dyjae@vip.qq.com>
2020-01-27 22:36:18 +0800

    feat 26 暂停菜单 AudioMixer https://www.bilibili.com/video/av68758224/

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject pauseMenu;
+
+    private bool isClose = false;
+
+    public Slider slider;
+
+    public AudioMixer audioMixer;
+
     public void PlayeGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -18,5 +28,35 @@ public class Menu : MonoBehaviour
     public void UIEnable()
     {
         GameObject.Find("Canvas/Menu/UI").SetActive(true);
+    }
+
+    public void Puase()
+    {
+        isClose = !isClose;
+        pauseMenu.SetActive(isClose);
+        PuaseGame();
+    }
+
+    public void ClosePuase()
+    {
+        isClose = false;
+        pauseMenu.SetActive(isClose);
+        PuaseGame();
+    }
+
+    private void PuaseGame()
+    {
+        if (isClose)
+        {
+            Time.timeScale = 0f;
+        }
+        else {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void SetVolume()
+    {
+        audioMixer.SetFloat("MainVolume", slider.value);
     }
 }
